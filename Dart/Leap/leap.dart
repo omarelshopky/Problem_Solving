74a87ea5afa551be23e2e8f842db9fbf6b82e6b8
omar hesham <69885253+omarhesham2@users.noreply.github.com>
2021-03-28 20:41:55 +0200

    Update leap.dart

--- Dart/Leap/leap.dart
+++ Dart/Leap/leap.dart
@@ -2,8 +2,8 @@ class Leap {
   bool leapYear(int year){
     if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
       return true;
-    }else{
-      return false;
     }
+
+    return false;
   }
 }
