b0f840bfdff13eb77af570f37df706ddacc85b81
omar hesham <69885253+omarhesham2@users.noreply.github.com>
2021-04-06 02:58:10 +0200

    Create scrabble-score.dart

---
+++ Dart/Scrabble_Score/scrabble-score.dart
@@ -0,0 +1,11 @@
+Map <String, int> points = {'A': 1, 'E':1 ,'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+                            'D':2, 'G':2, 'B':3, 'C':3, 'M':3, 'P':3, 'F':4, 'H':4, 'V':4, 'W':4, 'Y':4, 'K':5,
+                            'J':8, 'X':8, 'Q':10, 'Z':10};
+int score(String s){
+  int scores = 0;
+  for(int i = 0; i < s.length; i++){
+
+    scores += points[s[i].toUpperCase()];
+  }
+  return scores;
+}
